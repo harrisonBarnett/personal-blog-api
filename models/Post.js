c8b9943663c0ac6d24e13481537ec3540e50622c
Harrison <h.alexander.barnett@gmail.com>
2022-01-08 20:15:48 -0500

    adding lots of routes, styling edit page

--- models/Post.js
+++ models/Post.js
@@ -6,6 +6,7 @@ module.exports = mongoose.model(
         title: {type: String},
         content: {type: String},
         date_added: {type: Date},
-        date_modified: {type: Date}
+        date_modified: {type: Date},
+        is_published: {type: Boolean, default: false}
     })
 )
--- routes/index.js
+++ routes/index.js
@@ -2,10 +2,52 @@ var express = require('express');
 var router = express.Router();
 const Post = require('../models/Post')
 
+// GET login page
+router.get('/login', (req, res) => {
+  res.render('login')
+})
+// POST authenticate and grant access to the editor
+router.post('/login', (req, res) => {
+  res.redirect('/home')
+})
+// rerouting just in case
+router.get('/home',  (req, res) => {
+  Post.find({}).sort('-date_added')
+    .then(posts => {
+      res.render('home', {posts})
+    })
+    .catch(err => console.error(err))
+
+})
+// GET an editor for a single blog post AUTH
+router.get('/archive/:id', (req, res) => {
+  res.send('you are at the single post editor')
+})
+// UPDATE a single blog post AUTH
+router.put('/archive/:id', (req, res) => {
+  res.send('updating an existing blog post')
+})
+// POST a new blog AUTH
+router.post('/', (req, res) => {
+  const { title, content, publish } = req.body
+  const newPost = new Post({
+    title,
+    content,
+    date_added: Date.now(),
+    date_modified: Date.now(),
+    is_published: publish
+  })
+  newPost.save()
+    .then(()=>{
+      res.redirect('/home')
+    })
+    .catch(err => console.error(err))
+})
+// rerouting to page 1 just in case
 router.get('/', (req, res) => {
   res.redirect('/1')
 })
-
+// GET 5 posts at a time for personal site AUTH
 router.get('/:pageNo', (req, res) => {
   const resultsPerPage = 5
   const page = req.params.pageNo >= 1 ? req.params.pageNo : 1
@@ -20,19 +62,8 @@ router.get('/:pageNo', (req, res) => {
     .catch(err => console.error(err))
 })
 
-router.post('/', (req, res) => {
-  const { title, content } = req.body
-  const newPost = new Post({
-    title,
-    content,
-    date_added: Date.now(),
-    date_modified: Date.now()
-  })
-  newPost.save()
-    .then(()=>{
-      res.redirect('/')
-    })
-    .catch(err => console.error(err))
-})
+
+
+
 
 module.exports = router;
