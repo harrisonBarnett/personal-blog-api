80a3485ee75f44d0a1baf956c89324f2b2ab5fdb
harrisonBarnett <74162979+harrisonBarnett@users.noreply.github.com>
2022-01-11 13:24:40 -0500

    refactoring routes

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -6,29 +6,29 @@ const Post = require('../models/Post')
 // ROUTES FOR BLOG EDITING GUI
 // ****************************
 // GET login page
-router.get('/', (req, res) => {
+router.get('/login', (req, res) => {
     res.render('login')
-  })
-  // POST authenticate and grant access to the editor
-  router.post('/', (req, res) => {
-    res.redirect('/dashboard')
-  })
-  // rerouting just in case
-  router.get('/dashboard',  (req, res) => {
-    Post.find({}).sort('-date_added')
-      .then(posts => {
-        res.render('dashboard', {posts})
-      })
-      .catch(err => console.error(err))
-  
-  })
-  // GET an editor for a single blog post AUTH
-  router.get('/archive/:id', (req, res) => {
-    res.send('you are at the single post editor')
-  })
-  // UPDATE a single blog post AUTH
-  router.put('/archive/:id', (req, res) => {
-    res.send('updating an existing blog post')
-  })
+})
+// POST authenticate and grant access to the editor
+router.post('/login', (req, res) => {
+  res.redirect('/')
+})
+// GET the main dashboard
+router.get('/',  (req, res) => {
+  Post.find({}).sort('-date_added')
+    .then(posts => {
+      res.render('dashboard', {posts})
+    })
+    .catch(err => console.error(err))
 
-  module.exports = router
+})
+// GET an editor for a single blog post AUTH
+router.get('/archive/:id', (req, res) => {
+  res.send('you are at the single post editor')
+})
+// UPDATE a single blog post AUTH
+router.put('/archive/:id', (req, res) => {
+  res.send('updating an existing blog post')
+})
+
+module.exports = router
