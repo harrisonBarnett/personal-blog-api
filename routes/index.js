9033da05b66485deb9907bc1c2aaf4ac241488d3
harrisonBarnett <74162979+harrisonBarnett@users.noreply.github.com>
2022-01-11 13:25:40 -0500

    small refactor on index routes

--- routes/index.js
+++ routes/index.js
@@ -5,6 +5,20 @@ const Post = require('../models/Post')
 // ****************************
 // ROUTES FOR API INTERACTION
 // ****************************
+// GET 5 posts at a time for personal site AUTH
+router.get('/posts/:pageNo', (req, res) => {
+  const resultsPerPage = 5
+  const page = req.params.pageNo >= 1 ? req.params.pageNo : 1
+
+  Post.find({})
+    .sort('-date_added')
+    .limit(resultsPerPage)
+    .skip(resultsPerPage * (page - 1))
+    .then(posts => {
+      res.json({page, posts})
+    })
+    .catch(err => console.error(err))
+})
 // POST a new blog AUTH
 router.post('/posts', (req, res) => {
   const { title, content, publish } = req.body
@@ -18,20 +32,6 @@ router.post('/posts', (req, res) => {
   newPost.save()
     .then(()=>{
       res.redirect('/dashboard')
-    })
-    .catch(err => console.error(err))
-})
-// GET 5 posts at a time for personal site AUTH
-router.get('/posts/:pageNo', (req, res) => {
-  const resultsPerPage = 5
-  const page = req.params.pageNo >= 1 ? req.params.pageNo : 1
-
-  Post.find({})
-    .sort('-date_added')
-    .limit(resultsPerPage)
-    .skip(resultsPerPage * (page - 1))
-    .then(posts => {
-      res.json({page, posts})
     })
     .catch(err => console.error(err))
 })
